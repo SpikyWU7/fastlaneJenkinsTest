b12c2cd6594c32a1afdde258569d812a7dec6a77
SpikyWU7 <spikywu7@gmail.com>
2022-09-19 12:12:24 +0300

    testApp

---
+++ testAppJenkins/testAppJenkins/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testAppJenkins
+//
+//  Created by Spiky WU7 on 19.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testAppJenkins/testAppJenkins/testAppJenkinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppJenkinsApp.swift
+//  testAppJenkins
+//
+//  Created by Spiky WU7 on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct testAppJenkinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
